f7035120598d496ad52323ece438d730a2916992
abhay <abhay@192.168.0.3>
2019-03-31 22:38:07 +0530

    student

---
+++ GitApp/src/main/java/com/nt/beans/Student.java
@@ -0,0 +1,12 @@
+package com.nt.beans;
+
+public class Student {
+
+	public static void main(String[] args) {
+	     int roll=20;;
+	     String name="staym";
+	     System.out.println(roll+" "+name);
+
+	}
+
+}
